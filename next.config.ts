c441cf8557e8c6816d09d40b9ae29df87ade3349
Trip Planner <barreradionisio@gmail.com>
2026-07-06 22:53:06 -0700

    PWA configurada con manifest e iconos

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
